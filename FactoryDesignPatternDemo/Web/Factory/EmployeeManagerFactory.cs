dd4c6de2a5a61a9df07a0ca9333ff4cad248ea74
SumanHazra <SumanHazra@LAPTOP-T4SN7HU2>
2022-03-23 23:11:07 +0530

    Simple Factory Implementation

---
+++ FactoryDesignPatternDemo/Web/Factory/EmployeeManagerFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Web.BusinessLogicManager;
+
+namespace Web.Factory
+{
+    public class EmployeeManagerFactory
+    {
+        public IEmployeeManager GetEmployeeManager(int employeeTypeId)
+        {
+            IEmployeeManager returnValue = null;
+            if(employeeTypeId ==1)
+                returnValue = new PermanentEmployeeManager();
+            else if(employeeTypeId ==2)
+                returnValue = new ContractEmployeeManager();
+            return returnValue;
+        }
+    }
+}
